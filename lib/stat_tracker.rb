58e6ed681e9664dcd22aae6e267402de636f6efc
Richard LaBrecque <RichardlaBreque@Gmail.com>
2022-01-12 12:01:33 -0500

    Refactor: refactoring winningest team

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -294,40 +294,40 @@ class StatTracker
   end
 
   def winningest_coach (season_id)
-    game_ids_in_season = []
-    games_in_season = []
-    #season_hash = Hash.new { |hash, key| hash[key] =  }
-    #seasons listed in games csv @read_games
-    #if arg == season_id > games in that season
-    #count times each coach shows up
-    #if coach AND win +1 wins
-    #if coach and loss +1 games played
-    @read_games.each do |game|
-      if season_id == game.season
-        game_ids_in_season << game.game_id
-      end
-    end
-    game_ids_in_season.each do |game_id|
-      @read_game_teams.each do |game|
-        if game_id == game.game_id
-        games_in_season << game
-        end
-      end
-    end
-      #need to find coach win perecents for games_in_season
-      coach_games = Hash.new([])
-    games_in_season.each do |game|
-      if !coach_games.key?(game.head_coach)
-        coach_games[game.head_coach] = []
-        coach_games[game.head_coach] << game.result
-      elsif coach_games.key?(game.head_coach)
-        coach_games[game.head_coach] << game.result
-      end
-
-
-    end
+    # game_ids_in_season = []
+    # games_in_season = []
+    # #season_hash = Hash.new { |hash, key| hash[key] =  }
+    # #seasons listed in games csv @read_games
+    # #if arg == season_id > games in that season
+    # #count times each coach shows up
+    # #if coach AND win +1 wins
+    # #if coach and loss +1 games played
+    # @read_games.each do |game|
+    #   if season_id == game.season
+    #     game_ids_in_season << game.game_id
+    #   end
+    # end
+    # game_ids_in_season.each do |game_id|
+    #   @read_game_teams.each do |game|
+    #     if game_id == game.game_id
+    #     games_in_season << game
+    #     end
+    #   end
+    # end
+    #   #need to find coach win perecents for games_in_season
+    # coach_games = Hash.new([])
+    # games = games_in_season(season_id)
+    #   games.each do |game|
+    #     if !coach_games.key?(game.head_coach)
+    #       coach_games[game.head_coach] = []
+    #       coach_games[game.head_coach] << game.result
+    #     elsif coach_games.key?(game.head_coach)
+    #       coach_games[game.head_coach] << game.result
+    #     end
+    #   end
+    coaches_hash = coach_games(season_id)
     coach_win_percent = {}
-      coach_games.each do |coach, win_loss|
+      coaches_hash.each do |coach, win_loss|
         wins = 0
         played = 0
         win_loss.each do |result|
@@ -414,5 +414,38 @@ class StatTracker
   end
 
   def fewest_tackles (season_id)
+  end
+
+  #helpers
+  def games_in_season(season_id)
+    game_ids_in_season = []
+    games_in_season_array = []
+      @read_games.each do |game|
+        if season_id == game.season
+          game_ids_in_season << game.game_id
+        end
+      end
+    game_ids_in_season.each do |game_id|
+      @read_game_teams.each do |game|
+        if game_id == game.game_id
+          games_in_season_array << game
+        end
+      end
+    end
+    games_in_season_array
+    #binding.pry
+  end
+  def coach_games(season_id)
+    coach_games_hash = Hash.new([])
+    games = games_in_season(season_id)
+      games.each do |game|
+        if !coach_games_hash.key?(game.head_coach)
+          coach_games_hash[game.head_coach] = []
+          coach_games_hash[game.head_coach] << game.result
+        elsif coach_games_hash.key?(game.head_coach)
+          coach_games_hash[game.head_coach] << game.result
+        end
+      end
+      coach_games_hash
   end
 end
